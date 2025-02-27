c8e4996c89beb76dd9c5d0d95af7c5977a1a1100
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-02-27 20:50:20 +0800

    refactor(info): 修改 关于 界面 UI 排序

--- src/core.hpp
+++ src/core.hpp
@@ -301,12 +301,13 @@ namespace core {
         std::print( " -> 初始化 UI.\n" );
         ui.add_back( "                    [ 信  息 ]\n\n" )
           .add_back( " < 返回 ", exit, cpp_utils::console_value::text_foreground_green | cpp_utils::console_value::text_foreground_intensity )
-          .add_back( "\n[ 名称 ]\n\n " INFO_FULL_NAME "\n\n[ 版本 ]\n\n " INFO_VERSION "\n\n[ 仓库 ]\n" )
+          .add_back(
+            "\n[ 名称 ]\n\n " INFO_FULL_NAME "\n\n[ 版本 ]\n\n " INFO_VERSION "\n\n[ 许可证 & 版权 ]\n\n " INFO_LICENSE
+            "\n\n " INFO_COPYRIGHT "\n\n[ 仓库 ]\n" )
           .add_back(
             " " INFO_REPO_URL " ", visit_repo_webpage,
             cpp_utils::console_value::text_default | cpp_utils::console_value::text_foreground_intensity
               | cpp_utils::console_value::text_lvb_underscore )
-          .add_back( "\n[ 许可证 & 版权 ]\n\n " INFO_LICENSE " \n\n " INFO_COPYRIGHT )
           .show();
         return cpp_utils::console_ui::back;
     }
