638ee2a21d6061586a0a7c8b22b9a7dcd64a23e2
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-03-03 23:52:25 +0800

    v6.1.2

--- src/info.hpp
+++ src/info.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #define INFO_FULL_NAME  "Student Computer Lab Toolkit"
 #define INFO_SHORT_NAME "SCLTK"
-#define INFO_VERSION    "v6.1.1"
+#define INFO_VERSION    "v6.1.2"
 #define INFO_LICENSE    "MIT License"
 #define INFO_COPYRIGHT  "(C) 2023 - present MaxLHy0424."
 #define INFO_REPO_URL   "https://github.com/MaxLHy0424/SCLTK"
