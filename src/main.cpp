d7f576a959e30a0d64bb51c8b220cb6b79bebf80
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-03-09 14:29:00 +0800

    refactor: 同步 cpp_utils 接口更改

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,16 @@
 auto main() -> int
 {
     cpp_utils::ignore_console_exit_signal( true );
-    cpp_utils::fix_window_size( core::current_window_handle, true );
+    cpp_utils::fix_window_size( core::window_handle, true );
     cpp_utils::set_console_charset( core::charset_id );
     cpp_utils::set_console_title( INFO_SHORT_NAME );
-    cpp_utils::set_console_size( core::default_console_width, core::default_console_height );
-    cpp_utils::set_window_translucency( core::current_window_handle, 255 );
-    cpp_utils::enable_window_minimize_ctrl( core::current_window_handle, false );
-    cpp_utils::enable_window_maximize_ctrl( core::current_window_handle, false );
-    cpp_utils::enable_window_close_ctrl( core::current_window_handle, true );
-    cpp_utils::console_ui ui;
+    cpp_utils::set_console_size(
+      core::window_handle, core::std_output_handle, core::default_console_width, core::default_console_height );
+    cpp_utils::set_window_translucency( core::window_handle, 255 );
+    cpp_utils::enable_window_minimize_ctrl( core::window_handle, false );
+    cpp_utils::enable_window_maximize_ctrl( core::window_handle, false );
+    cpp_utils::enable_window_close_ctrl( core::window_handle, true );
+    cpp_utils::console_ui ui{ core::std_input_handle, core::std_output_handle };
     ui.lock( true, true );
     std::print( " -> 检测运行权限.\n" );
     if ( !cpp_utils::is_run_as_admin() ) {
