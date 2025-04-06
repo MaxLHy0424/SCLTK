103cd16f89c520567c4b83148f57f2094a7a8965
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-04-06 13:31:38 +0800

    feat: 修改 工具箱 内置功能

--- src/core.hpp
+++ src/core.hpp
@@ -427,7 +427,7 @@ namespace core {
         };
         constexpr const ansi_char *common_ops[][ 2 ]{
           {"重启资源管理器",               R"(taskkill.exe /f /im explorer.exe && timeout.exe /t 3 /nobreak && start C:\Windows\explorer.exe)"},
-          {"修复操作系统",                 "dism.exe /online /cleanup-image /restorehealth && sfc.exe /scannow"                               },
+          {"重启至高级选项",               "shutdown.exe /r /o /f /t 15"                                                                      },
           {"恢复 USB 设备访问",            R"(reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR" /f /t reg_dword /v Start /d 3)"    },
           {"恢复 Google Chrome 离线游戏",  R"(reg.exe delete "HKLM\SOFTWARE\Policies\Google\Chrome" /f /v AllowDinosaurEasterEgg)"            },
           {"恢复 Microsoft Edge 离线游戏", R"(reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Edge" /f /v AllowSurfGame)"                    }
