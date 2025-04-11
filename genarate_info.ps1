118f19f65bda727a1cd18bed92bf5d92a5b791e8
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-04-12 07:27:59 +0800

    chore

--- genarate_info.ps1
+++ genarate_info.ps1
@@ -3,9 +3,9 @@ $software_short_name = "SCLTK"
 $license = "MIT License"
 $coprright = "Copyright (C) 2023 - present MaxLHy0424."
 $repo_url = "https://github.com/MaxLHy0424/SCLTK"
-$git_hash = git rev-parse --short HEAD
 $git_branch = git branch --show-current
 $git_tag = git describe --tags --abbrev=0
+$git_hash = git rev-parse --short HEAD
 $git_date = git log -1 --format=%cd --date=format:"%Y/%m/%d %H:%M:%S"
 $build_time = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
 $time_zone = Get-TimeZone | Select-Object -ExpandProperty Id
@@ -16,9 +16,9 @@ $time_zone = Get-TimeZone | Select-Object -ExpandProperty Id
 #define INFO_LICENSE    "$license"
 #define INFO_COPYRIGHT  "$coprright"
 #define INFO_REPO_URL   "$repo_url"
-#define INFO_GIT_HASH   "$git_hash"
 #define INFO_GIT_BRANCH "$git_branch"
 #define INFO_GIT_TAG    "$git_tag"
+#define INFO_GIT_HASH   "$git_hash"
 #define INFO_GIT_DATE   "$git_date"
 #define INFO_BUILD_TIME "$build_time"
 #define INFO_TIME_ZONE  "$time_zone"
