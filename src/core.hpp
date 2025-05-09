99e5b35be8692e88751800d29eed7dcbd423888c
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-05-09 21:03:02 +0800

    refactor

--- src/core.hpp
+++ src/core.hpp
@@ -357,7 +357,7 @@ namespace core {
             cpp_utils::set_console_size( window_handle, std_output_handle, 120, 30 );
             cpp_utils::fix_window_size( window_handle, false );
             cpp_utils::enable_window_maximize_ctrl( window_handle, true );
-            _args.parent_ui.lock( false, false );
+            _args.parent_ui.set_limits( false, false );
             SetConsoleScreenBufferSize( std_output_handle, COORD{ 127, SHRT_MAX - 1 } );
             std::system( "cmd.exe" );
             cpp_utils::set_console_charset( charset_id );
