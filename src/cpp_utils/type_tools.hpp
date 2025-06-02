b9c192f26a83cd2a79e89d7cd4a8cc57055e155e
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-06-02 21:57:06 +0800

    refactor!: 大重构

--- src/cpp_utils/type_tools.hpp
+++ src/cpp_utils/type_tools.hpp
@@ -2,10 +2,10 @@
 #include <cstddef>
 #include <type_traits>
 namespace cpp_utils {
-    using size_type    = std::size_t;
-    using nullptr_type = std::nullptr_t;
+    using size_t    = std::size_t;
+    using nullptr_t = std::nullptr_t;
     template < typename T >
     using type_set = T;
     template < typename T >
-    using add_const_lvalue_reference_type = std::add_lvalue_reference_t< std::add_const_t< T > >;
+    using add_const_lvalue_reference_t = std::add_lvalue_reference_t< std::add_const_t< T > >;
 }
