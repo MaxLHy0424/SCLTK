cc4ae6408f66fcd0e3318ca962e1643f158636d1
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-06-02 21:57:38 +0800

    feat: 调整 "关于" 界面

--- make_info.ps1
+++ make_info.ps1
@@ -6,11 +6,11 @@ $repo_url = "https://github.com/MaxLHy0424/SCLTK"
 $git_branch = git branch --show-current
 $git_tag = git describe --tags --abbrev=0
 $git_hash = git rev-parse --short HEAD
-$git_date = git log -1 --format=%cd --date=format:"%Y/%m/%d %H:%M:%S"
-$build_time = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
-$time_zone = Get-TimeZone | Select-Object -ExpandProperty Id
+$build_time = Get-Date -AsUTC -Format "yyyy/MM/dd HH:mm:ss"
 @"
 #pragma once
+#define STRINGIFY( x ) #x
+#define TO_STRING( x ) STRINGIFY( x )
 #define INFO_FULL_NAME  "$software_full_name"
 #define INFO_SHORT_NAME "$software_short_name"
 #define INFO_LICENSE    "$license"
@@ -19,8 +19,22 @@ $time_zone = Get-TimeZone | Select-Object -ExpandProperty Id
 #define INFO_GIT_BRANCH "$git_branch"
 #define INFO_GIT_TAG    "$git_tag"
 #define INFO_GIT_HASH   "$git_hash"
-#define INFO_GIT_DATE   "$git_date"
-#define INFO_BUILD_TIME "$build_time"
-#define INFO_TIME_ZONE  "$time_zone"
+#define INFO_BUILD_TIME "UTC " "$build_time"
 #define INFO_VERSION    INFO_GIT_TAG " (" INFO_GIT_BRANCH " " INFO_GIT_HASH ")"
+#if defined( __LP64__ ) || defined( _WIN64 )
+# define INFO_ARCH "x86_64"
+#else
+# define INFO_ARCH "i686"
+#endif
+#if defined( __clang__ )
+# define INFO_COMPILER "clang " TO_STRING( __clang_major__ ) "." TO_STRING( __clang_minor__ ) "." TO_STRING( __clang_patchlevel__ )
+#elif defined( __GNUC__ )
+# define INFO_COMPILER "gcc " TO_STRING( __GNUC__ ) "." TO_STRING( __GNUC_MINOR__ ) "." TO_STRING( __GNUC_PATCHLEVEL__ )
+#elif defined( _MSC_VER )
+# define INFO_COMPILER "msvc " TO_STRING( _MSC_VER )
+#else
+# define INFO_COMPILER "{unknow}"
+#undef TO_STRING
+#undef STRINGIFY
+#endif
 "@ | Out-File -FilePath "src/info.hpp" -Encoding UTF8 -NoNewline -Force
