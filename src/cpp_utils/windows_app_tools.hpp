ce47d76718bc4b0c10eb2bbc1f5108a9d998aa09
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-06-13 21:31:51 +0800

    feat: 添加服务管理和字符串转换功能，增强 Windows 应用工具的实用性

--- src/cpp_utils/windows_app_tools.hpp
+++ src/cpp_utils/windows_app_tools.hpp
@@ -1,13 +1,16 @@
 #pragma once
 #if defined( _WIN32 ) || defined( _WIN64 )
 # include "windows_definations.hpp"
+# if true
+#  include <tlhelp32.h>
+# endif
 #endif
 #include <chrono>
 #include <concepts>
-#include <deque>
 #include <functional>
+#include <memory>
 #include <print>
-#include <ranges>
+#include <string>
 #include <thread>
 #include <type_traits>
 #include <utility>
@@ -15,6 +18,279 @@
 namespace cpp_utils
 {
 #if defined( _WIN32 ) || defined( _WIN64 )
+    namespace details
+    {
+        template < UINT Charset >
+        inline auto to_wstring( const char* const str ) noexcept
+        {
+            using namespace std::string_literals;
+            if ( str == nullptr ) {
+                return L""s;
+            }
+            if ( str[ 0 ] == '\0' ) {
+                return L""s;
+            }
+            const auto size_needed{ MultiByteToWideChar( Charset, 0, str, -1, nullptr, 0 ) };
+            if ( size_needed <= 0 ) {
+                return L""s;
+            }
+            std::wstring result( size_needed, 0 );
+            MultiByteToWideChar( Charset, 0, str, -1, result.data(), size_needed );
+            return result;
+        }
+        inline auto stop_service_and_dependencies( const SC_HANDLE scm, const SC_HANDLE service ) noexcept -> DWORD
+        {
+            using namespace std::chrono_literals;
+            DWORD result{ ERROR_SUCCESS };
+            SERVICE_STATUS status;
+            DWORD bytes_needed{ 0 };
+            if ( !QueryServiceConfigW( service, nullptr, 0, &bytes_needed ) && GetLastError() == ERROR_INSUFFICIENT_BUFFER ) {
+                const auto buffer{ std::make_unique< BYTE[] >( bytes_needed ) };
+                const auto config{ reinterpret_cast< LPQUERY_SERVICE_CONFIGW >( buffer.get() ) };
+                if ( QueryServiceConfigW( service, config, bytes_needed, &bytes_needed ) ) {
+                    if ( config->lpDependencies && *config->lpDependencies ) {
+                        auto dep{ config->lpDependencies };
+                        while ( *dep != L'\0' ) {
+                            const auto dep_svc{ OpenServiceW( scm, dep, SERVICE_STOP | SERVICE_QUERY_STATUS ) };
+                            if ( dep_svc != nullptr ) {
+                                const auto dep_result{ stop_service_and_dependencies( scm, dep_svc ) };
+                                if ( dep_result != ERROR_SUCCESS ) {
+                                    result = dep_result;
+                                }
+                                CloseServiceHandle( dep_svc );
+                            }
+                            dep += wcslen( dep ) + 1;
+                        }
+                    }
+                }
+            }
+            if ( ControlService( service, SERVICE_CONTROL_STOP, &status ) ) {
+                while ( QueryServiceStatus( service, &status ) ) {
+                    if ( status.dwCurrentState == SERVICE_STOP_PENDING ) {
+                        std::this_thread::sleep_for( 50ms );
+                    } else {
+                        break;
+                    }
+                }
+                if ( status.dwCurrentState != SERVICE_STOPPED ) {
+                    result = ERROR_SERVICE_REQUEST_TIMEOUT;
+                }
+            } else if ( GetLastError() != ERROR_SERVICE_NOT_ACTIVE ) {
+                result = GetLastError();
+            }
+            return result;
+        }
+        inline auto start_service_and_dependencies( const SC_HANDLE scm, const SC_HANDLE service ) noexcept -> DWORD
+        {
+            DWORD result{ ERROR_SUCCESS };
+            DWORD bytes_needed;
+            if ( QueryServiceConfigW( service, nullptr, 0, &bytes_needed ) || GetLastError() == ERROR_INSUFFICIENT_BUFFER ) {
+                const auto buffer{ std::make_unique< BYTE[] >( bytes_needed ) };
+                const auto config{ reinterpret_cast< LPQUERY_SERVICE_CONFIGW >( buffer.get() ) };
+                if ( QueryServiceConfigW( service, config, bytes_needed, &bytes_needed ) ) {
+                    if ( config->lpDependencies && *config->lpDependencies ) {
+                        wchar_t* context{ nullptr };
+                        auto dep{ wcstok_s( config->lpDependencies, L"\0", &context ) };
+                        while ( dep != nullptr ) {
+                            if ( *dep != L'@' ) {  // 忽略服务组
+                                const auto dep_svc{ OpenServiceW( scm, dep, SERVICE_START | SERVICE_QUERY_STATUS ) };
+                                if ( dep_svc ) {
+                                    SERVICE_STATUS status;
+                                    if ( !QueryServiceStatus( dep_svc, &status )
+                                         || ( status.dwCurrentState != SERVICE_RUNNING && status.dwCurrentState != SERVICE_START_PENDING ) )
+                                    {
+                                        result = start_service_and_dependencies( scm, dep_svc );
+                                    }
+                                    CloseServiceHandle( dep_svc );
+                                }
+                            }
+                            dep = wcstok_s( nullptr, L"\0", &context );
+                        }
+                    }
+                }
+            }
+            if ( result == ERROR_SUCCESS && !StartServiceW( service, 0, nullptr ) ) {
+                const auto err{ GetLastError() };
+                if ( err != ERROR_SERVICE_ALREADY_RUNNING ) {
+                    result = err;
+                }
+            }
+            return result;
+        }
+    }
+    template < UINT Charset >
+    inline auto kill_process_by_name( const char* const process_name ) noexcept
+    {
+        const auto w_name{ details::to_wstring< Charset >( process_name ) };
+        if ( w_name.empty() ) {
+            return static_cast< DWORD >( ERROR_INVALID_PARAMETER );
+        }
+        const auto snapshot{ CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 ) };
+        if ( snapshot == INVALID_HANDLE_VALUE ) {
+            return GetLastError();
+        }
+        PROCESSENTRY32W pe{};
+        pe.dwSize = sizeof( pe );
+        DWORD result{ ERROR_SUCCESS };
+        bool is_found{ false };
+        if ( Process32FirstW( snapshot, &pe ) ) {
+            do {
+                if ( _wcsicmp( pe.szExeFile, w_name.c_str() ) == 0 ) {
+                    is_found                  = true;
+                    const auto process_handle = OpenProcess( PROCESS_TERMINATE, FALSE, pe.th32ProcessID );
+                    if ( process_handle ) {
+                        if ( !TerminateProcess( process_handle, 1 ) ) {
+                            result = GetLastError();
+                        }
+                        CloseHandle( process_handle );
+                    } else {
+                        result = GetLastError();
+                    }
+                }
+            } while ( Process32NextW( snapshot, &pe ) );
+        } else {
+            result = GetLastError();
+        }
+        CloseHandle( snapshot );
+        return is_found ? result : ERROR_NOT_FOUND;
+    }
+    template < UINT Charset >
+    inline auto create_registry_key(
+      const HKEY main_key, const char* const sub_key, const char* const value_name, const DWORD type, const BYTE* const data,
+      const DWORD data_size ) noexcept
+    {
+        using namespace std::string_literals;
+        const auto w_sub_key{ details::to_wstring< Charset >( sub_key ) };
+        const auto w_value_name{ value_name ? details::to_wstring< Charset >( value_name ) : L""s };
+        HKEY key;
+        auto result{ RegCreateKeyExW(
+          main_key, w_sub_key.c_str(), 0, nullptr, REG_OPTION_NON_VOLATILE, KEY_WRITE, nullptr, &key, nullptr ) };
+        if ( result != ERROR_SUCCESS ) {
+            return result;
+        }
+        result = RegSetValueExW( key, w_value_name.empty() ? nullptr : w_value_name.c_str(), 0, type, data, data_size );
+        RegCloseKey( key );
+        return result;
+    }
+    template < UINT Charset >
+    inline auto delete_registry_key( const HKEY main_key, const char* const sub_key, const char* const value_name ) noexcept
+    {
+        using namespace std::string_literals;
+        const auto w_sub_key{ details::to_wstring< Charset >( sub_key ) };
+        const auto w_value_name{ value_name ? details::to_wstring< Charset >( value_name ) : L""s };
+        HKEY hKey;
+        auto result{ RegOpenKeyExW( main_key, w_sub_key.c_str(), 0, KEY_WRITE, &hKey ) };
+        if ( result != ERROR_SUCCESS ) {
+            return result;
+        }
+        result = RegDeleteValueW( hKey, w_value_name.empty() ? nullptr : w_value_name.c_str() );
+        RegCloseKey( hKey );
+        return result;
+    }
+    template < UINT Charset >
+    inline auto delete_registry_tree( const HKEY main_key, const char* const sub_key ) noexcept
+    {
+        return RegDeleteTreeW( main_key, details::to_wstring< Charset >( sub_key ).c_str() );
+    }
+    template < UINT Charset >
+    inline auto disable_service( const char* const service_name ) noexcept
+    {
+        const auto w_name{ details::to_wstring< Charset >( service_name ) };
+        if ( w_name.empty() ) {
+            return static_cast< DWORD >( ERROR_INVALID_PARAMETER );
+        }
+        const auto scm{ OpenSCManagerW( nullptr, nullptr, SC_MANAGER_CONNECT ) };
+        if ( scm == nullptr ) {
+            return GetLastError();
+        }
+        const auto service{ OpenServiceW( scm, w_name.c_str(), SERVICE_CHANGE_CONFIG ) };
+        DWORD result{ ERROR_SUCCESS };
+        if ( service != nullptr ) {
+            if ( !ChangeServiceConfigW(
+                   service, SERVICE_NO_CHANGE, SERVICE_DISABLED, SERVICE_NO_CHANGE, nullptr, nullptr, nullptr, nullptr, nullptr,
+                   nullptr, nullptr ) )
+            {
+                result = GetLastError();
+            }
+            CloseServiceHandle( service );
+        } else {
+            result = GetLastError();
+        }
+        CloseServiceHandle( scm );
+        return result;
+    }
+    template < UINT Charset >
+    inline auto enable_service( const char* const service_name ) noexcept
+    {
+        const auto w_name{ details::to_wstring< Charset >( service_name ) };
+        if ( w_name.empty() ) {
+            return static_cast< DWORD >( ERROR_INVALID_PARAMETER );
+        }
+        const auto scm{ OpenSCManagerW( nullptr, nullptr, SC_MANAGER_CONNECT ) };
+        if ( scm == nullptr ) {
+            return GetLastError();
+        }
+        const auto service{ OpenServiceW( scm, w_name.c_str(), SERVICE_CHANGE_CONFIG ) };
+        DWORD result{ ERROR_SUCCESS };
+        if ( service ) {
+            if ( !ChangeServiceConfigW(
+                   service, SERVICE_NO_CHANGE, SERVICE_DEMAND_START, SERVICE_NO_CHANGE, nullptr, nullptr, nullptr, nullptr,
+                   nullptr, nullptr, nullptr ) )
+            {
+                result = GetLastError();
+            }
+            CloseServiceHandle( service );
+        } else {
+            result = GetLastError();
+        }
+        CloseServiceHandle( scm );
+        return result;
+    }
+    template < UINT Charset >
+    inline auto stop_service_with_dependencies( const char* const service_name ) noexcept
+    {
+        const auto w_name{ details::to_wstring< Charset >( service_name ) };
+        if ( w_name.empty() ) {
+            return static_cast< DWORD >( ERROR_INVALID_PARAMETER );
+        }
+        const auto scm{ OpenSCManagerW( nullptr, nullptr, SC_MANAGER_CONNECT | SC_MANAGER_ENUMERATE_SERVICE ) };
+        if ( scm == nullptr ) {
+            return GetLastError();
+        }
+        const auto service{
+          OpenServiceW( scm, w_name.c_str(), SERVICE_STOP | SERVICE_QUERY_STATUS | SERVICE_ENUMERATE_DEPENDENTS ) };
+        DWORD result{ ERROR_SUCCESS };
+        if ( service ) {
+            result = details::stop_service_and_dependencies( scm, service );
+            CloseServiceHandle( service );
+        } else {
+            result = GetLastError();
+        }
+        CloseServiceHandle( scm );
+        return result;
+    }
+    template < UINT Charset >
+    inline auto start_service_with_dependencies( const char* const service_name ) noexcept
+    {
+        const auto w_name{ details::to_wstring< Charset >( service_name ) };
+        if ( w_name.empty() ) {
+            return static_cast< DWORD >( ERROR_INVALID_PARAMETER );
+        }
+        const auto scm{ OpenSCManagerW( nullptr, nullptr, SC_MANAGER_CONNECT ) };
+        if ( scm == nullptr ) {
+            return GetLastError();
+        }
+        const auto service{ OpenServiceW( scm, w_name.c_str(), SERVICE_START | SERVICE_QUERY_STATUS | SERVICE_QUERY_CONFIG ) };
+        DWORD result{ ERROR_SUCCESS };
+        if ( service != nullptr ) {
+            result = details::start_service_and_dependencies( scm, service );
+            CloseServiceHandle( service );
+        } else {
+            result = GetLastError();
+        }
+        CloseServiceHandle( scm );
+        return result;
+    }
     inline auto is_run_as_admin() noexcept
     {
         BOOL is_admin;
@@ -106,7 +382,7 @@ namespace cpp_utils
         requires std::invocable< F, Args... >
     inline auto loop_keep_window_top(
       const HWND window_handle, const DWORD thread_id, const DWORD window_thread_process_id,
-      const std::chrono::duration< ChronoRep, ChronoPeriod > sleep_time, F &&condition_checker, Args &&...condition_checker_args )
+      const std::chrono::duration< ChronoRep, ChronoPeriod > sleep_time, F&& condition_checker, Args&&... condition_checker_args )
     {
         while ( condition_checker( std::forward< Args >( condition_checker_args )... ) ) {
             AttachThreadInput( thread_id, window_thread_process_id, TRUE );
@@ -125,7 +401,7 @@ namespace cpp_utils
     template < typename ChronoRep, typename ChronoPeriod, typename F, typename... Args >
         requires std::invocable< F, Args... >
     inline auto loop_keep_current_window_top(
-      const std::chrono::duration< ChronoRep, ChronoPeriod > sleep_time, F &&condition_checker, Args &&...condition_checker_args )
+      const std::chrono::duration< ChronoRep, ChronoPeriod > sleep_time, F&& condition_checker, Args&&... condition_checker_args )
     {
         const auto window_handle{ get_current_window_handle() };
         loop_keep_window_top(
@@ -173,19 +449,19 @@ namespace cpp_utils
     {
         reset_console( GetStdHandle( STD_OUTPUT_HANDLE ) );
     }
-    inline auto set_current_console_title( const char *const title ) noexcept
+    inline auto set_current_console_title( const char* const title ) noexcept
     {
         SetConsoleTitleA( title );
     }
-    inline auto set_current_console_title( const std::string &title ) noexcept
+    inline auto set_current_console_title( const std::string& title ) noexcept
     {
         SetConsoleTitleA( title.data() );
     }
-    inline auto set_current_console_title( const wchar_t *const title ) noexcept
+    inline auto set_current_console_title( const wchar_t* const title ) noexcept
     {
         SetConsoleTitleW( title );
     }
-    inline auto set_current_console_title( const std::wstring &title ) noexcept
+    inline auto set_current_console_title( const std::wstring& title ) noexcept
     {
         SetConsoleTitleW( title.data() );
     }
