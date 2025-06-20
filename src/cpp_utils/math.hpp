aa25e2f1d983c1c34811de73fc21be330de63f25
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-06-20 18:53:10 +0800

    fix: 修复 count_digits 函数参数类型，提升类型安全性

--- src/cpp_utils/math.hpp
+++ src/cpp_utils/math.hpp
@@ -20,13 +20,14 @@ namespace cpp_utils
         return true;
     }
     template < std::integral T >
-    inline constexpr auto count_digits( T n ) noexcept
+    inline constexpr auto count_digits( const T n ) noexcept
     {
+        using result_t = unsigned short;
         T abs_n{ n < 0 ? -n : n };
         if ( abs_n == 0 ) {
-            return static_cast< size_t >( 1 );
+            return static_cast< result_t >( 1 );
         }
-        size_t count{ 0 };
+        result_t count{ 0 };
         while ( abs_n > 0 ) {
             abs_n /= 10;
             ++count;
