ca4d19be6297f12f12ad3f5dab491934e96d4651
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-06-26 14:51:50 +0800

    refactor!: 拆分 meta 为 meta_base 和 meta_type

--- src/cpp_utils/meta_type.hpp
+++ src/cpp_utils/meta_type.hpp
@@ -1,16 +1,8 @@
 #pragma once
-#include <cstddef>
 #include <tuple>
-#include <type_traits>
-#include <utility>
+#include "meta_base.hpp"
 namespace cpp_utils
 {
-    using size_t    = std::size_t;
-    using nullptr_t = std::nullptr_t;
-    template < typename T >
-    using type_alloc = T;
-    template < typename T >
-    using add_const_lvalue_reference_t = std::add_lvalue_reference_t< std::add_const_t< T > >;
     template < bool Expr >
     concept test = Expr;
     template < typename... Ts >
