6899e469fed2c5f087b7ffd320a23d36c033a999
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-07-01 08:16:02 +0800

    feat(cpp_utils/compiler): 引入 version 标准库头文件，添加 RTTI 检测和 Exceptions 检测

--- src/cpp_utils/compiler.hpp
+++ src/cpp_utils/compiler.hpp
@@ -1,9 +1,22 @@
 #pragma once
+#include <version>
 namespace cpp_utils
 {
 #ifndef NDEBUG
     constexpr auto is_debugging_build{ true };
 #else
     constexpr auto is_debugging_build{ false };
+#endif
+#if defined( __GXX_RTTI ) || ( defined( _CPPRTTI ) && _CPPRTTI )
+    constexpr auto has_rtti{ true };
+#else
+    constexpr auto has_rtti{ false };
+#endif
+
+// 检测异常
+#if ( defined( __EXCEPTIONS ) && __EXCEPTIONS ) || ( defined( _CPPUNWIND ) && _CPPUNWIND )
+    constexpr auto has_exceptions{ true };
+#else
+    constexpr auto has_exceptions{ false };
 #endif
 }
