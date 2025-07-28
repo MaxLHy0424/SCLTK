59c17ec52a8599fea274649eb9ac2ee824c60dda
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-07-28 10:11:28 +0800

    feat(cpp_utils/math): 添加 sat_num 类

--- include/cpp_utils/math.hpp
+++ include/cpp_utils/math.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <cmath>
 #include <concepts>
-#include <vector>
+#include <numeric>
 namespace cpp_utils
 {
     template < std::integral T >
@@ -35,4 +35,82 @@ namespace cpp_utils
         }
         return count;
     }
+    template < typename T >
+        requires std::integral< T > || std::floating_point< T > || std::same_as< T, std::decay< T > >
+    class sat_num final
+    {
+      private:
+        T data_;
+      public:
+        constexpr auto base() const noexcept
+        {
+            return data_;
+        }
+        constexpr auto operator<=>( const sat_num< T >& src ) const noexcept
+        {
+            return data_ <=> src.data_;
+        }
+        constexpr auto operator+( const sat_num< T > src ) const noexcept
+        {
+            return sat_num< T >{ std::add_sat( data_, src.data_ ) };
+        }
+        constexpr auto operator-( const sat_num< T > src ) const noexcept
+        {
+            return sat_num< T >{ std::sub_sat( data_, src.data_ ) };
+        }
+        constexpr auto operator*( const sat_num< T > src ) const noexcept
+        {
+            return sat_num< T >{ std::mul_sat( data_, src.data_ ) };
+        }
+        constexpr auto operator/( const sat_num< T > src ) const noexcept
+        {
+            return sat_num< T >{ std::div_sat( data_, src.data_ ) };
+        }
+        constexpr auto& operator+=( const sat_num< T > src ) noexcept
+        {
+            data_ = std::add_sat( data_, src.data_ );
+            return *this;
+        }
+        constexpr auto& operator-=( const sat_num< T > src ) noexcept
+        {
+            data_ = std::sub_sat( data_, src.data_ );
+            return *this;
+        }
+        constexpr auto& operator*=( const sat_num< T > src ) noexcept
+        {
+            data_ = std::mul_sat( data_, src.data_ );
+            return *this;
+        }
+        constexpr auto& operator/=( const sat_num< T > src ) noexcept
+        {
+            data_ = std::div_sat( data_, src.data_ );
+            return *this;
+        }
+        constexpr auto& operator=( const sat_num< T >& src ) noexcept
+        {
+            data_ = src.data_;
+            return *this;
+        }
+        constexpr auto& operator=( sat_num< T >&& src ) noexcept
+        {
+            data_     = src.data_;
+            src.data_ = {};
+            return *this;
+        }
+        constexpr sat_num( const T n )
+          : data_{ n }
+        { }
+        constexpr sat_num( const sat_num< T >& src )
+          : data_{ src.data_ }
+        { }
+        constexpr sat_num( sat_num< T >&& src )
+          : data_{ src.data_ }
+        {
+            src.data_ = {};
+        }
+        ~sat_num() = default;
+    };
+    template < typename T >
+        requires std::integral< T > || std::floating_point< T > || std::same_as< T, std::decay< T > >
+    sat_num( T ) -> sat_num< T >;
 }
