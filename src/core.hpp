4ca0cc05ad1a66b016e4aafc4ae70a2702c7e6dd
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-09-07 09:45:54 +0800

    refactor

--- src/core.hpp
+++ src/core.hpp
@@ -821,8 +821,8 @@ namespace core
             const auto& options{ std::get< crack_restore_config >( config_nodes ) };
             const auto can_hijack_execs{ options[ "hijack_execs" ].get() };
             const auto can_set_serv_startup_types{ options[ "set_serv_startup_types" ].get() };
-            const auto total_op_count{ 2 + can_hijack_execs + can_set_serv_startup_types };
-            unsigned finished_count{ 0 };
+            const auto total_op_count{ 2u + can_hijack_execs + can_set_serv_startup_types };
+            auto finished_count{ 0u };
             if ( can_hijack_execs ) {
                 std::print( " ({}/{}) 劫持文件.\n", ++finished_count, total_op_count );
                 for ( const auto& exec : execs ) {
@@ -874,8 +874,8 @@ namespace core
             const auto& options{ std::get< crack_restore_config >( config_nodes ) };
             const auto can_hijack_execs{ options[ "hijack_execs" ].get() };
             const auto can_set_serv_startup_types{ options[ "set_serv_startup_types" ].get() };
-            const auto total_op_count{ 1 + can_hijack_execs + can_set_serv_startup_types };
-            unsigned finished_count{ 0 };
+            const auto total_op_count{ 1u + can_hijack_execs + can_set_serv_startup_types };
+            auto finished_count{ 0u };
             if ( can_hijack_execs ) {
                 std::print( " ({}/{}) 撤销劫持.\n", ++finished_count, total_op_count );
                 for ( const auto& exec : execs ) {
