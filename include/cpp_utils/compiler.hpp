eb4378c4cc2a617c24cf84864ebd2f037701eaba
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-09-11 21:58:02 +0800

    refactor

--- include/cpp_utils/compiler.hpp
+++ include/cpp_utils/compiler.hpp
@@ -3,18 +3,18 @@
 namespace cpp_utils
 {
 #ifndef NDEBUG
-    constexpr auto is_debugging_build{ true };
+    inline constexpr auto is_debugging_build{ true };
 #else
-    constexpr auto is_debugging_build{ false };
+    inline constexpr auto is_debugging_build{ false };
 #endif
 #if defined( __GXX_RTTI ) || ( defined( _CPPRTTI ) && _CPPRTTI )
-    constexpr auto has_rtti{ true };
+    inline constexpr auto has_rtti{ true };
 #else
-    constexpr auto has_rtti{ false };
+    inline constexpr auto has_rtti{ false };
 #endif
 #if ( defined( __EXCEPTIONS ) && __EXCEPTIONS ) || ( defined( _CPPUNWIND ) && _CPPUNWIND )
-    constexpr auto has_exceptions{ true };
+    inline constexpr auto has_exceptions{ true };
 #else
-    constexpr auto has_exceptions{ false };
+    inline constexpr auto has_exceptions{ false };
 #endif
 }
