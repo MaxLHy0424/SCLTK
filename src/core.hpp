8c1210c847c145fb23c4d13c814dad70484d3aea
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-09-13 00:37:43 +0800

    refactor(cpp_utils/windows_app_tools): 修改 clear console

--- src/core.hpp
+++ src/core.hpp
@@ -1014,7 +1014,7 @@ namespace core
             total.execs.append_range( builtin_rule.execs );
             total.servs.append_range( builtin_rule.servs );
         }
-        cpp_utils::clear_console_traditional( std_output_handle );
+        cpp_utils::clear_console( std_output_handle );
         return execute_rules( total );
     }
 }
