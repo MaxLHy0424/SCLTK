55ebd1e00150fc902c3d4a7f45346a8528988c49
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-10-02 19:49:24 +0800

    build: 将 env 更名为 env.mk

--- make_env.ps1
+++ make_env.ps1
@@ -1,4 +1,4 @@
 @"
 msys2_path = /path/to/msys2
 pwsh_path  = /path/to/pwsh
-"@ | Out-File -FilePath "env" -Encoding UTF8 -NoNewline -Force
+"@ | Out-File -FilePath "env.mk" -Encoding UTF8 -NoNewline -Force
