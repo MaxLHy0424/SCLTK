fe19504e609e31e46ebafe33e7052ee6c9d86611
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-02-01 22:27:31 +0800

    build: make_info 将生成可读性更好的 gpg key id 串

--- make_info.ps1
+++ make_info.ps1
@@ -20,6 +20,7 @@ if ($contains_uncommitted_changes -eq $true ) {
 else {
     $git_hash = "<work in progress>"
 }
+$pretty_gpg_key_id = [regex]::Replace($gpg_key_id, '.{1,5}', { param($m) $m.Value + ' ' }).Trim()
 @"
 #pragma once
 #define INFO_FULL_NAME  "$software_full_name"
@@ -31,5 +32,5 @@ else {
 #define INFO_GIT_TAG    "$git_tag"
 #define INFO_GIT_HASH   "$git_hash"
 #define INFO_VERSION    INFO_GIT_TAG " (" INFO_GIT_BRANCH " " INFO_GIT_HASH ")"
-#define INFO_GPG_KEY    "$gpg_key_id"
+#define INFO_GPG_KEY    "$pretty_gpg_key_id"
 "@ | Out-File -FilePath "src/info.hpp" -Encoding UTF8 -NoNewline -Force
