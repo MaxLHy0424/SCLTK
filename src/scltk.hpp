d34953f6867993c14e1c4fc631fefb5f7827fc39
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-02-01 19:16:03 +0800

    refactor(scltk/rule_executor/restore): 使用 constexpr 引用

--- src/scltk.hpp
+++ src/scltk.hpp
@@ -961,8 +961,8 @@ namespace scltk
         static auto restore()
         {
             constexpr const auto& options{ std::get< crack_restore_config >( config_nodes ) };
-            const auto can_hijack_execs{ options.at< "hijack_execs" >() };
-            const auto can_set_serv_startup_types{ options.at< "set_serv_startup_types" >() };
+            constexpr const auto& can_hijack_execs{ options.at< "hijack_execs" >() };
+            constexpr const auto& can_set_serv_startup_types{ options.at< "set_serv_startup_types" >() };
             if ( can_hijack_execs ) {
                 std::print( " -> 撤销劫持.\n" );
                 ( Backends::undo_hijack_execs(), ... );
