338a80a3fab17dc49c1e3a7143d87875c4785fe1
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-02-01 22:00:54 +0800

    chore: 修正错误描述

--- make_env.ps1
+++ make_env.ps1
@@ -2,5 +2,5 @@
 msys2_path = /path/to/msys2
 pwsh_path  = /path/to/pwsh
 gpg_path   = /path/to/gpg
-gpg_key    = private-key-id
+gpg_key    = key-id
 "@ | Out-File -FilePath "env.mk" -Encoding UTF8 -NoNewline -Force
