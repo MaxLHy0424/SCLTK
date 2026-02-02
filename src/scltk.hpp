817aedfa46259c7a5698e34c3822a339b9ec5316
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-02-02 17:57:11 +0800

    refactor(scltk/toolkit/reset_firewall_rules): 在 CreateProcessW 后添加创建情况检测

--- src/scltk.hpp
+++ src/scltk.hpp
@@ -765,13 +765,15 @@ namespace scltk
             si.hStdOutput = nul_file_handle;
             si.hStdError  = nul_file_handle;
             wchar_t cmd[]{ L"netsh.exe advfirewall reset" };
-            CreateProcessW(
+            const auto has_created_process_successfully{ CreateProcessW(
               nullptr, cmd, nullptr, nullptr, TRUE, CREATE_NO_WINDOW | CREATE_UNICODE_ENVIRONMENT, nullptr, nullptr, &si,
-              &proc_info );
+              &proc_info ) };
             CloseHandle( nul_file_handle );
-            WaitForSingleObject( proc_info.hProcess, INFINITE );
-            CloseHandle( proc_info.hProcess );
-            CloseHandle( proc_info.hThread );
+            if ( has_created_process_successfully ) {
+                WaitForSingleObject( proc_info.hProcess, INFINITE );
+                CloseHandle( proc_info.hProcess );
+                CloseHandle( proc_info.hThread );
+            }
         }
         inline auto reset_hosts() noexcept
         {
