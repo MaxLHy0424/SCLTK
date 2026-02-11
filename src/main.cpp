046ed9a1cc1d9636fecb003f4ba513af3e0b9676
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-02-11 20:54:39 +0800

    refactor(scltk/start_screen): 优化启动初始化流程

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,22 @@
 auto main() -> int
 {
     using namespace std::string_view_literals;
+    scltk::con.set_charset( scltk::charset_id );
+    std::print( " -> 准备就绪." );
     scltk::con.ignore_exit_signal( true )
-      .fix_size( true )
-      .set_charset( scltk::charset_id )
       .set_title( L"" INFO_SHORT_NAME )
+      .show_cursor( false )
+      .fix_size( true )
+      .lock_text( true )
       .set_size( scltk::console_width, scltk::console_height, scltk::unsynced_mem_pool )
       .set_translucency( 255 )
       .enable_window_maximize_ctrl( false )
       .enable_window_minimize_ctrl( false )
-      .enable_window_close_ctrl( false )
-      .show_cursor( false )
-      .lock_text( true );
-    cpp_utils::console_ui ui{ scltk::con, scltk::unsynced_mem_pool };
-    std::print( " -> 准备就绪." );
+      .enable_window_close_ctrl( false );
     scltk::elevate_privilege();
     scltk::load_config( false );
     scltk::create_parallel_tasks();
+    cpp_utils::console_ui ui{ scltk::con, scltk::unsynced_mem_pool };
     ui.reserve( 9 + scltk::builtin_rules_t::size )
       .add_back( scltk::make_title_text< "[ 主  页 ]", 2 >.view() )
       .add_back( " < 退出 "sv, scltk::quit, cpp_utils::console_text::foreground_red | cpp_utils::console_text::foreground_intensity )
