71fe2b9b39c9f444937279a22b7bd7f751c7c95e
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-02-25 16:34:07 +0800

    chore

--- make.ps1
+++ make.ps1
@@ -47,4 +47,4 @@ elseif ((Get-FileHash $old_info).Hash -ne (Get-FileHash $new_info).Hash ) {
     Copy-Item -Path $new_info -Destination $old_info
 }
 Remove-Item -Path "build/info.gen"
-& make $target -f main.mk -j gpg_key=$gpg_key
+return (& make $target -f main.mk -j gpg_key=$gpg_key)
