064cc8694d6749c1d848d657baac55339e031bf0
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-04-04 14:35:14 +0800

    build: 修改缩进

--- main.mk
+++ main.mk
@@ -1,65 +1,65 @@
-include_path            := include
-cpp_utils_path          := $(include_path)/cpp_utils
+include_path          := include
+cpp_utils_path        := $(include_path)/cpp_utils
 include $(cpp_utils_path)/all.mk
-project_name            := SCLTK
-x86_64_compiler         := /ucrt64/bin/g++
-i686_compiler           := /mingw32/bin/g++
-args_arch_32            := -march=pentium4 -mtune=generic -msse2 -mfpmath=sse
-args_arch_64            := -march=x86-64 -mtune=generic -msse3 -mfpmath=sse
-args_defines            := -D{UNICODE,_UNICODE}
-args_std                := gnu++26
-args_warning            := -W{all,extra,effc++,pedantic,cast-align,logical-op,redundant-decls,shadow,strict-null-sentinel}
-args_opt_debug          := -Og -fno-omit-frame-pointer
-args_opt_release_base   := -Ofast \
-                           -fno-use-linker-plugin \
-                           -fwhole-program \
-                           -s \
-                           -fvisibility=hidden \
-                           -fvisibility-inlines-hidden \
-                           -fno-rtti \
-                           -fno-exceptions \
-                           -fno-unwind-tables \
-                           -fno-asynchronous-unwind-tables \
-                           -fno-common \
-                           -ffunction-sections \
-                           -fdata-sections \
-                           -fno-semantic-interposition \
-                           -fdevirtualize-at-ltrans \
-                           -fdevirtualize-speculatively \
-                           -fipa-pta \
-                           -fipa-ra \
-                           -fipa-icf \
-                           -fomit-frame-pointer \
-                           -fno-plt
-args_opt_release_32     := $(args_opt_release_base)
-args_opt_release_64     := $(args_opt_release_base) -flto=auto
-args_include            := -I$(include_path)
-args_library            := -liphlpapi -lsetupapi
-args_extra              :=
-input_charset           := utf-8
-output_charset          := gbk
-args_base               := -pipe -finput-charset=$(input_charset) -fexec-charset=$(output_charset) \
-                           -std=$(args_std) $(args_warning) $(args_defines) $(args_include) \
-                           $(args_library) $(args_extra)
-args_debug              := -g3 -fuse-ld=lld -DDEBUG $(args_base) $(args_opt_debug) -fstack-protector-strong
-args_release_32         := -DNDEBUG -static $(args_base) $(args_opt_release_32)
-args_release_64         := -DNDEBUG -static $(args_base) $(args_opt_release_64)
-args_ld_base            := -fuse-ld=lld -Wl,-O3,--lto-O3,--lto-CGO3,--gc-sections,--strip-all,--as-needed,--no-insert-timestamp,--no-seh,--disable-runtime-pseudo-reloc,--disable-auto-import,--dynamicbase,--nxcompat,--high-entropy-va,--tsaware,--icf=all
-args_ld_32              := $(args_ld_base)
-args_ld_64              := $(args_ld_base)
-cmd_echo                := /usr/bin/echo
-cmd_upx                 := /ucrt64/bin/upx --lzma --best --8-bit --no-align --ultra-brute -qqq
-cmd_gpg                 := gpg -bs -u $(gpg_key) --yes
-dep_test                := src/main.cpp src/info.hpp $(cpp_utils_all_files)
-dep_debug               := src/*.cpp
-dep_release_32          := build/manifest-i686.o \
-                           src/*.cpp
-dep_release_64          := build/manifest-x86_64.o \
-                           src/*.cpp
-dep_res                 := manifest.rc \
-                           img/favicon.ico \
-                           manifest.xml \
-                           src/info.hpp
+project_name          := SCLTK
+x86_64_compiler       := /ucrt64/bin/g++
+i686_compiler         := /mingw32/bin/g++
+args_arch_32          := -march=pentium4 -mtune=generic -msse2 -mfpmath=sse
+args_arch_64          := -march=x86-64 -mtune=generic -msse3 -mfpmath=sse
+args_defines          := -D{UNICODE,_UNICODE}
+args_std              := gnu++26
+args_warning          := -W{all,extra,effc++,pedantic,cast-align,logical-op,redundant-decls,shadow,strict-null-sentinel}
+args_opt_debug        := -Og -fno-omit-frame-pointer
+args_opt_release_base := -Ofast \
+                         -fno-use-linker-plugin \
+                         -fwhole-program \
+                         -s \
+                         -fvisibility=hidden \
+                         -fvisibility-inlines-hidden \
+                         -fno-rtti \
+                         -fno-exceptions \
+                         -fno-unwind-tables \
+                         -fno-asynchronous-unwind-tables \
+                         -fno-common \
+                         -ffunction-sections \
+                         -fdata-sections \
+                         -fno-semantic-interposition \
+                         -fdevirtualize-at-ltrans \
+                         -fdevirtualize-speculatively \
+                         -fipa-pta \
+                         -fipa-ra \
+                         -fipa-icf \
+                         -fomit-frame-pointer \
+                         -fno-plt
+args_opt_release_32   := $(args_opt_release_base)
+args_opt_release_64   := $(args_opt_release_base) -flto=auto
+args_include          := -I$(include_path)
+args_library          := -liphlpapi -lsetupapi
+args_extra            :=
+input_charset         := utf-8
+output_charset        := gbk
+args_base             := -pipe -finput-charset=$(input_charset) -fexec-charset=$(output_charset) \
+                         -std=$(args_std) $(args_warning) $(args_defines) $(args_include) \
+                         $(args_library) $(args_extra)
+args_debug            := -g3 -fuse-ld=lld -DDEBUG $(args_base) $(args_opt_debug) -fstack-protector-strong
+args_release_32       := -DNDEBUG -static $(args_base) $(args_opt_release_32)
+args_release_64       := -DNDEBUG -static $(args_base) $(args_opt_release_64)
+args_ld_base          := -fuse-ld=lld -Wl,-O3,--lto-O3,--lto-CGO3,--gc-sections,--strip-all,--as-needed,--no-insert-timestamp,--no-seh,--disable-runtime-pseudo-reloc,--disable-auto-import,--dynamicbase,--nxcompat,--high-entropy-va,--tsaware,--icf=all
+args_ld_32            := $(args_ld_base)
+args_ld_64            := $(args_ld_base)
+cmd_echo              := /usr/bin/echo
+cmd_upx               := /ucrt64/bin/upx --lzma --best --8-bit --no-align --ultra-brute -qqq
+cmd_gpg               := gpg -bs -u $(gpg_key) --yes
+dep_test              := src/main.cpp src/info.hpp $(cpp_utils_all_files)
+dep_debug             := src/*.cpp
+dep_release_32        := build/manifest-i686.o \
+                         src/*.cpp
+dep_release_64        := build/manifest-x86_64.o \
+                         src/*.cpp
+dep_res               := manifest.rc \
+                         img/favicon.ico \
+                         manifest.xml \
+                         src/info.hpp
 .PHONY: toolchain all build debug release release-32 release-64 pack_and_sign clean
 .NOTPARALLEL: all
 all: toolchain build pack_and_sign
