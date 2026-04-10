66ec7afa7e3f93e2b296901475ca6208c4ba8eee
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-04-10 20:43:23 +0800

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -103,115 +103,6 @@ namespace scltk
             }
             return false;
         }
-        auto terminate_jfglzs_daemon() noexcept
-        {
-            using scoped_handle = std::unique_ptr< std::remove_pointer_t< HANDLE >, decltype( []( const HANDLE handle ) static noexcept
-            {
-                if ( handle != INVALID_HANDLE_VALUE ) {
-                    CloseHandle( handle );
-                }
-            } ) >;
-            const scoped_handle process_snapshot{ CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 ) };
-            if ( process_snapshot.get() == INVALID_HANDLE_VALUE ) [[unlikely]] {
-                return;
-            }
-            constexpr auto is_lower_case{ []( const wchar_t ch ) static noexcept { return ch >= L'a' && ch <= L'z'; } };
-            constexpr const auto& needle{ L"Program Files" };
-            constexpr auto needle_begin{ std::ranges::begin( needle ) };
-            constexpr auto needle_end{ std::ranges::end( needle ) - 1 };
-            const std::boyer_moore_horspool_searcher searcher{ needle_begin, needle_end };
-            PROCESSENTRY32W process_entry{};
-            process_entry.dwSize = sizeof( process_entry );
-            if ( !Process32FirstW( process_snapshot.get(), &process_entry ) ) [[unlikely]] {
-                return;
-            }
-            do {
-                std::wstring_view name{ process_entry.szExeFile };
-                if ( name.size() != L"xxxxx.exe"sv.size() ) {
-                    continue;
-                }
-                name.remove_suffix( L".exe"sv.size() );
-                if ( std::ranges::all_of( name, is_lower_case ) ) {
-                    scoped_handle process_handle{
-                      OpenProcess( PROCESS_TERMINATE | PROCESS_QUERY_LIMITED_INFORMATION, FALSE, process_entry.th32ProcessID ) };
-                    if ( process_handle == nullptr ) [[unlikely]] {
-                        continue;
-                    }
-                    DWORD size{ MAX_PATH };
-                    std::array< wchar_t, MAX_PATH > buffer{};
-                    QueryFullProcessImageNameW( process_handle.get(), 0, buffer.data(), &size );
-                    if ( std::search( buffer.begin(), buffer.end(), searcher ) != buffer.end() ) {
-                        TerminateProcess( process_handle.get(), 1 );
-                    }
-                }
-            } while ( Process32NextW( process_snapshot.get(), &process_entry ) );
-        }
-        auto terminate_workwin() noexcept
-        {
-            using file_path_type = std::array< wchar_t, MAX_PATH >;
-            constexpr auto is_sign_match{ []( const file_path_type& path ) static noexcept
-            {
-                using scoped_cert_store
-                  = std::unique_ptr< std::remove_pointer_t< HCERTSTORE >, decltype( []( const HCERTSTORE h ) static noexcept
-                { CertCloseStore( h, 0 ); } ) >;
-                HCERTSTORE cert_store{ nullptr };
-                DWORD encoding{ 0 };
-                DWORD content_type{ 0 };
-                DWORD format_type{ 0 };
-                if ( !CryptQueryObject(
-                       CERT_QUERY_OBJECT_FILE, path.data(), CERT_QUERY_CONTENT_FLAG_PKCS7_SIGNED_EMBED,
-                       CERT_QUERY_FORMAT_FLAG_BINARY, 0, &encoding, &content_type, &format_type, &cert_store, nullptr, nullptr )
-                     || !cert_store ) [[unlikely]]
-                {
-                    return false;
-                }
-                scoped_cert_store cert_store_guard{ cert_store };
-                PCCERT_CONTEXT cert{ nullptr };
-                constexpr std::wstring_view target{ L"Nanjing Wangya Computer Co.,Ltd." };
-                while ( ( cert = CertEnumCertificatesInStore( cert_store, cert ) ) != nullptr ) {
-                    DWORD name_len{ CertGetNameStringW( cert, CERT_NAME_SIMPLE_DISPLAY_TYPE, 0, nullptr, nullptr, 0 ) };
-                    if ( name_len < 2 ) [[unlikely]] {
-                        continue;
-                    }
-                    std::pmr::vector< wchar_t > name_buf{ name_len, unsynced_mem_pool };
-                    CertGetNameStringW( cert, CERT_NAME_SIMPLE_DISPLAY_TYPE, 0, nullptr, name_buf.data(), name_len );
-                    if ( target == name_buf.data() ) {
-                        return true;
-                    }
-                }
-                return false;
-            } };
-            using scoped_handle = std::unique_ptr< std::remove_pointer_t< HANDLE >, decltype( []( const HANDLE h ) static noexcept
-            {
-                if ( h != INVALID_HANDLE_VALUE ) {
-                    CloseHandle( h );
-                }
-            } ) >;
-            scoped_handle proc_snapshot{ CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 ) };
-            if ( proc_snapshot.get() == INVALID_HANDLE_VALUE ) {
-                return;
-            }
-            PROCESSENTRY32W proc_entry{};
-            proc_entry.dwSize = sizeof( PROCESSENTRY32W );
-            if ( !Process32FirstW( proc_snapshot.get(), &proc_entry ) ) {
-                return;
-            }
-            do {
-                scoped_handle proc_handle{
-                  OpenProcess( PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_TERMINATE, FALSE, proc_entry.th32ProcessID ) };
-                if ( proc_handle == nullptr || proc_handle.get() == INVALID_HANDLE_VALUE ) {
-                    continue;
-                }
-                file_path_type path{};
-                DWORD size{ MAX_PATH };
-                if ( !QueryFullProcessImageNameW( proc_handle.get(), 0, path.data(), &size ) ) [[unlikely]] {
-                    continue;
-                }
-                if ( is_sign_match( path ) ) {
-                    TerminateProcess( proc_handle.get(), 1 );
-                }
-            } while ( Process32NextW( proc_snapshot.get(), &proc_entry ) );
-        }
     }
     template < cpp_utils::const_string DisplayName, cpp_utils::same_as_type_list Procs, cpp_utils::same_as_type_list Servs,
                std::invocable auto CrackHelper = details_::empty_lambda, std::invocable auto RestoreHelper = details_::empty_lambda >
@@ -1173,6 +1064,115 @@ namespace scltk
         };
         auto current_rule_executor_mode{ rule_executor_mode::crack };
         constexpr std::array mythware_servs{ L"STUDSRV"sv, L"TDKeybd"sv, L"TDNetFilter"sv, L"TDFileFilter"sv };
+        auto terminate_jfglzs_daemon() noexcept
+        {
+            using scoped_handle = std::unique_ptr< std::remove_pointer_t< HANDLE >, decltype( []( const HANDLE handle ) static noexcept
+            {
+                if ( handle != INVALID_HANDLE_VALUE ) {
+                    CloseHandle( handle );
+                }
+            } ) >;
+            const scoped_handle process_snapshot{ CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 ) };
+            if ( process_snapshot.get() == INVALID_HANDLE_VALUE ) [[unlikely]] {
+                return;
+            }
+            constexpr auto is_lower_case{ []( const wchar_t ch ) static noexcept { return ch >= L'a' && ch <= L'z'; } };
+            constexpr const auto& needle{ L"Program Files" };
+            constexpr auto needle_begin{ std::ranges::begin( needle ) };
+            constexpr auto needle_end{ std::ranges::end( needle ) - 1 };
+            const std::boyer_moore_horspool_searcher searcher{ needle_begin, needle_end };
+            PROCESSENTRY32W process_entry{};
+            process_entry.dwSize = sizeof( process_entry );
+            if ( !Process32FirstW( process_snapshot.get(), &process_entry ) ) [[unlikely]] {
+                return;
+            }
+            do {
+                std::wstring_view name{ process_entry.szExeFile };
+                if ( name.size() != L"xxxxx.exe"sv.size() ) {
+                    continue;
+                }
+                name.remove_suffix( L".exe"sv.size() );
+                if ( std::ranges::all_of( name, is_lower_case ) ) {
+                    scoped_handle process_handle{
+                      OpenProcess( PROCESS_TERMINATE | PROCESS_QUERY_LIMITED_INFORMATION, FALSE, process_entry.th32ProcessID ) };
+                    if ( process_handle == nullptr ) [[unlikely]] {
+                        continue;
+                    }
+                    DWORD size{ MAX_PATH };
+                    std::array< wchar_t, MAX_PATH > buffer{};
+                    QueryFullProcessImageNameW( process_handle.get(), 0, buffer.data(), &size );
+                    if ( std::search( buffer.begin(), buffer.end(), searcher ) != buffer.end() ) {
+                        TerminateProcess( process_handle.get(), 1 );
+                    }
+                }
+            } while ( Process32NextW( process_snapshot.get(), &process_entry ) );
+        }
+        auto terminate_workwin() noexcept
+        {
+            using file_path_type = std::array< wchar_t, MAX_PATH >;
+            constexpr auto is_sign_match{ []( const file_path_type& path ) static noexcept
+            {
+                using scoped_cert_store
+                  = std::unique_ptr< std::remove_pointer_t< HCERTSTORE >, decltype( []( const HCERTSTORE h ) static noexcept
+                { CertCloseStore( h, 0 ); } ) >;
+                HCERTSTORE cert_store{ nullptr };
+                DWORD encoding{ 0 };
+                DWORD content_type{ 0 };
+                DWORD format_type{ 0 };
+                if ( !CryptQueryObject(
+                       CERT_QUERY_OBJECT_FILE, path.data(), CERT_QUERY_CONTENT_FLAG_PKCS7_SIGNED_EMBED,
+                       CERT_QUERY_FORMAT_FLAG_BINARY, 0, &encoding, &content_type, &format_type, &cert_store, nullptr, nullptr )
+                     || !cert_store ) [[unlikely]]
+                {
+                    return false;
+                }
+                scoped_cert_store cert_store_guard{ cert_store };
+                PCCERT_CONTEXT cert{ nullptr };
+                constexpr std::wstring_view target{ L"Nanjing Wangya Computer Co.,Ltd." };
+                while ( ( cert = CertEnumCertificatesInStore( cert_store, cert ) ) != nullptr ) {
+                    DWORD name_len{ CertGetNameStringW( cert, CERT_NAME_SIMPLE_DISPLAY_TYPE, 0, nullptr, nullptr, 0 ) };
+                    if ( name_len < 2 ) [[unlikely]] {
+                        continue;
+                    }
+                    std::pmr::vector< wchar_t > name_buf{ name_len, unsynced_mem_pool };
+                    CertGetNameStringW( cert, CERT_NAME_SIMPLE_DISPLAY_TYPE, 0, nullptr, name_buf.data(), name_len );
+                    if ( target == name_buf.data() ) {
+                        return true;
+                    }
+                }
+                return false;
+            } };
+            using scoped_handle = std::unique_ptr< std::remove_pointer_t< HANDLE >, decltype( []( const HANDLE h ) static noexcept
+            {
+                if ( h != INVALID_HANDLE_VALUE ) {
+                    CloseHandle( h );
+                }
+            } ) >;
+            scoped_handle proc_snapshot{ CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 ) };
+            if ( proc_snapshot.get() == INVALID_HANDLE_VALUE ) {
+                return;
+            }
+            PROCESSENTRY32W proc_entry{};
+            proc_entry.dwSize = sizeof( PROCESSENTRY32W );
+            if ( !Process32FirstW( proc_snapshot.get(), &proc_entry ) ) {
+                return;
+            }
+            do {
+                scoped_handle proc_handle{
+                  OpenProcess( PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_TERMINATE, FALSE, proc_entry.th32ProcessID ) };
+                if ( proc_handle == nullptr || proc_handle.get() == INVALID_HANDLE_VALUE ) {
+                    continue;
+                }
+                file_path_type path{};
+                DWORD size{ MAX_PATH };
+                if ( !QueryFullProcessImageNameW( proc_handle.get(), 0, path.data(), &size ) ) [[unlikely]] {
+                    continue;
+                }
+                if ( is_sign_match( path ) ) {
+                    TerminateProcess( proc_handle.get(), 1 );
+                }
+            } while ( Process32NextW( proc_snapshot.get(), &proc_entry ) );
+        }
     }
     using builtin_rules = cpp_utils::type_list<
       compile_time_rule_node<
