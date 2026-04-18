ee329c3e1a6e2429fdb1671ed08fbb85dc6a6f52
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-04-18 08:06:55 +0800

    refactor(cpp_utils/diagnostics)!: 修改 dynamic_assert 实现

--- include/cpp_utils/diagnostics.hpp
+++ include/cpp_utils/diagnostics.hpp
@@ -7,6 +7,7 @@
 #include <stacktrace>
 #include <string_view>
 #include <utility>
+#include "compiler.hpp"
 namespace cpp_utils
 {
     [[nodiscard]] inline auto make_log(
@@ -17,14 +18,30 @@ namespace cpp_utils
           "{}({}:{}) {}: {}\n{}\n", src_location.file_name(), src_location.line(), src_location.column(),
           src_location.function_name(), message, trace );
     }
+    template < typename F >
+        requires requires( F&& func ) {
+            { std::forward< F >( func )()() } -> std::convertible_to< bool >;
+        }
     inline auto dynamic_assert(
-      const bool expression, const std::string_view failed_message = "assertion failid!",
-      const std::source_location src_location = std::source_location::current(),
-      std::pmr::stacktrace trace              = std::pmr::stacktrace::current() )
+      F&& func, const std::source_location src_location = std::source_location::current(),
+      std::pmr::stacktrace trace = std::pmr::stacktrace::current() ) noexcept( has_exceptions )
     {
-        if ( expression == false ) [[unlikely]] {
-            std::print( stderr, "{}", make_log( failed_message, src_location, std::move( trace ) ) );
+        using namespace std::string_view_literals;
+#ifdef CPP_UTILS_MACRO_HAS_EXCEPTIONS
+        try {
+#endif
+            if ( std::forward< F >( func )() == false ) [[unlikely]] {
+                std::print( stderr, "{}", make_log( "assertion failid!"sv, src_location, std::move( trace ) ) );
+                std::terminate();
+            }
+#ifdef CPP_UTILS_MACRO_HAS_EXCEPTIONS
+        } catch ( const std::exception& e ) {
+            std::print( stderr, "{}", make_log( e.what(), src_location, std::move( trace ) ) );
+            std::terminate();
+        } catch ( ... ) {
+            std::print( stderr, "{}", make_log( "unknown exception caught!"sv, src_location, std::move( trace ) ) );
             std::terminate();
         }
+#endif
     }
 }
