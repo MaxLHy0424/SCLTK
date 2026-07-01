50df82fe3124d7d16a28f55497cacad8f37fc95b
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-07-01 15:38:19 +0800

    feat

--- include/cpp_utils/pointer.hpp
+++ include/cpp_utils/pointer.hpp
@@ -24,7 +24,8 @@ namespace cpp_utils
       private:
         T ptr_{};
       public:
-        using value_type = T;
+        using value_type   = T;
+        using checker_type = NullChecker;
         constexpr auto computable() const noexcept
         {
             return !std::is_same_v< std::remove_cv_t< std::remove_pointer_t< T > >, void >;
