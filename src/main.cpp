8118e500fae941d94257a61d6f057463154f5592
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-08-23 12:28:41 +0800

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1533,8 +1533,8 @@ namespace scltk
                 cpp_utils::print_without_formatting(
                   "\n"
                   " (i) 由于 Windows 系统限制,\n"
-                  "     WOW64 下无法重启网络适配器,\n"
-                  "     请手动重启网络适配器.\n"sv );
+                  "     无法重启网络适配器,\n"
+                  "     请手动重启.\n"sv );
                 return;
             }
 #endif
