fdc164f33d2cf2343ef6d1a62c047f972c0a86c9
MaxLHy0424 <MaxLHy974413@outlook.com>
2026-09-05 16:14:18 +0800

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -55,8 +55,9 @@ namespace scltk
     }
     auto enable_privileges() noexcept
     {
-        const auto current_process{ GetCurrentProcess() };
-        for ( const auto privilege : { L"" SE_DEBUG_NAME, L"" SE_SHUTDOWN_NAME } ) {
+        for ( const auto current_process{ GetCurrentProcess() };
+              const auto privilege : { L"" SE_DEBUG_NAME, L"" SE_SHUTDOWN_NAME } )
+        {
             ( void ) cpp_utils::set_privilege( current_process, privilege, true );
         }
     }
